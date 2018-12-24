bf9d378aa50e887da54460f59083f99e9448e8b5
Ayaka Hibino <ayaka.hibino@gmail.com>
2018-12-24 02:30:13 -0500

    final changes

--- js/script.js
+++ js/script.js
@@ -1,35 +1,24 @@
-
 var eventType = prompt('What type of event are you going to? Choose from the following: casual, semi-formal, and formal.');
 var tempFahr = prompt('How many degrees Fahrenheit is it outside?');
-var para = document.querySelector('p');
+
+if (eventType === 'casual') {
+    eventClothing ='something comfy';
+} else if (eventType === 'semi-formal') {
+    eventClothing = 'a polo';
+} else if (eventType === 'formal') {
+    eventClothing = 'a suit';
+} else {eventClothing = 'Not an option.';}
 
 
+if (tempFahr < 54) {
+    tempClothing ='a coat';
+} else if (tempFahr >= 54 && tempFahr <= 70) {
+    tempClothing = 'a jacket';
+} else if (tempFahr > 70) {
+    tempClothing = 'no jacket';
+} else {tempClothing = 'Not an option.';}
 
-if (eventType === 'casual' && tempFahr < 54) {
-    para.textContent = 'Since it is' + tempFahr + 'degrees and you are going to a' + eventType + 'event, you should wear something comfy and a coat.';
-   
-} else if (eventType === 'casual' && (tempFahr >= 54) || (tempFahr <= 70) {
-    para.textContent = 'Since it is' + tempFahr + 'degrees and you are going to a' + eventType + 'event, you should wear something comfy and a jacket.';
-    
-} else if (eventType === 'casual' && tempFahr > 70) {
-    para.textContent = 'Since it is' + tempFahr + 'degrees and you are going to a' + eventType + 'event, you should wear something comfy and no jacket.';
+var result = 'Since it is ' + tempFahr + ' degrees and you are going to a ' + eventType + ' event, you should wear ' + eventClothing + ' and ' + tempClothing + '.'
 
-} else if (eventType === 'semi-formal' && tempFahr < 54) {
-    para.textContent = 'Since it is' + tempFahr + 'degrees and you are going to a' + eventType + 'event, you should wear a polo and a coat.';
-       
-} else if (eventType === 'semi-formal' && (tempFahr >= 54) || (tempFahr <= 70) {
-    para.textContent = 'Since it is' + tempFahr + 'degrees and you are going to a' + eventType + 'event, you should wear a polo and a jacket.';
-        
-} else if (eventType === 'semi-formal' && tempFahr > 70) {
-    para.textContent = 'Since it is' + tempFahr + 'degrees and you are going to a' + eventType + 'event, you should wear a polo and no jacket.';
-
-} else if (eventType === 'formal' && tempFahr < 54) {
-    para.textContent = 'Since it is' + tempFahr + 'degrees and you are going to a' + eventType + 'event, you should wear a suit and a coat.';
-       
-} else if (eventType === 'formal' && (tempFahr >= 54) || (tempFahr <= 70) {
-    para.textContent = 'Since it is' + tempFahr + 'degrees and you are going to a' + eventType + 'event, you should wear a suit and a jacket.';
-        
-} else if (eventType === 'formal' && tempFahr > 70) {
-    para.textContent = 'Since it is' + tempFahr + 'degrees and you are going to a' + eventType + 'event, you should wear a suit and no jacket.';
-    
-}
+console.log(result);
+ 
